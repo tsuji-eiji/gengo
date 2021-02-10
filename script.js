86e4c526a4332a4de89d57d1b1a71d20927b9168
tsuji-eiji <eiji.rock24@gmail.com>
2021-02-10 19:06:45 +0900

    一旦完成

--- script.js
+++ script.js
@@ -72,6 +72,10 @@ document.getElementById('submit2').addEventListener('click', () => {
 	let inputYear = document.getElementById('year2').value;
 	let inputMonth = document.getElementById('month2').value;
 	let inputDay = document.getElementById('day2').value;
+	if (inputYear === '元') {
+		inputYear = '01';
+	}
+	
 
 	//入力チェック
 	const inputDate = new Date(inputYear, inputMonth - 1, inputDay);
@@ -80,28 +84,67 @@ document.getElementById('submit2').addEventListener('click', () => {
 		return false;
 	}
 
+	//ゼロ埋め
+	//年
+	if (inputYear.length < 2) {
+		inputYear = zeroPadding(inputYear, 2);
+	}
+	//月
+	if (inputMonth.length < 2) {
+		inputMonth = zeroPadding(inputMonth, 2);
+	}
+	//日
+	if (inputDay.length < 2) {
+		inputDay = zeroPadding(inputDay, 2);
+	}
 
-	let out = '';
-	switch(inputNengo){
+
+	const jdgnum = Number(inputNengo + inputYear + inputMonth + inputDay);
+	switch (inputNengo) {
 		case '1':
-			output('令和',2018);
+			if (jdgnum < 1010501) {
+				document.getElementById('result2').innerHTML = '令和は元年5月1日からです。';
+				return false;
+			}
+			output('令和', 2018);
 			break;
 		case '2':
-			output('平成',1988);
+			if (jdgnum < 2010108 || 2310430 < jdgnum) {
+				document.getElementById('result2').innerHTML = '平成は元年1月8日から31年4月30日までです。';
+				return false;
+			}
+			output('平成', 1988);
 			break;
 		case '3':
-			output('昭和',1925);
+			if (jdgnum < 3011225 || 3640107 < jdgnum) {
+				document.getElementById('result2').innerHTML = '昭和は元年12月25日から64年1月7日までです。';
+				return false;
+			}
+			output('昭和', 1925);
 			break;
 		case '4':
-			output('大正',1911);
+			if (jdgnum < 4010730 || 4151224 < jdgnum) {
+				document.getElementById('result2').innerHTML = '大正は元年7月30日から15年12月24日までです。';
+				return false;
+			}
+			output('大正', 1911);
 			break;
 		case '5':
-			output('明治',1867);
+			if (jdgnum < 5010101 || 5450729 < jdgnum) {
+				document.getElementById('result2').innerHTML = '明治は元年1月1日から45年7月29日までです。';
+				return false;
+			}
+			output('明治', 1867);
 			break;
 	}
-	
-	function output(nengo,addYear){
-		let out = `<strong>${nengo}${inputYear}年${inputMonth}月${inputDay}日</strong>は<br><strong>西暦${Number(inputYear) + addYear}年${inputMonth}月${inputDay}日</strong>です。`
+
+	function output(nengo, addYear) {
+		let out = '';
+		if (inputYear === '01') {
+			out = `<strong>${nengo}元年${inputMonth}月${inputDay}日</strong>は<br><strong>西暦${Number(inputYear) + addYear}年${inputMonth}月${inputDay}日</strong>です。`;
+		} else {
+			out = `<strong>${nengo}${inputYear}年${inputMonth}月${inputDay}日</strong>は<br><strong>西暦${Number(inputYear) + addYear}年${inputMonth}月${inputDay}日</strong>です。`;
+		}
 		document.getElementById('result2').innerHTML = `<p>${out}</p>`;
 	}
 })
